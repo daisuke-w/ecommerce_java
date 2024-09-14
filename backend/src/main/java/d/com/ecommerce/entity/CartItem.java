e62ce0de61115986bf13e91982c2b429c06063a8
daisuke-w <w.daisuke.001@gmail.com>
2024-09-14 18:19:06 +0900

    CartItemEntity作成

---
+++ backend/src/main/java/d/com/ecommerce/entity/CartItem.java
@@ -0,0 +1,27 @@
+package d.com.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class CartItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name = "usert_id")
+	private User user;
+	
+	private int quantity;
+}
