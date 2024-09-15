4ffc6c0886198e9d9e4ee2162f4e35db86a8e1a8
daisuke-w <w.daisuke.001@gmail.com>
2024-09-15 20:38:27 +0900

    テストコード修正

--- backend/src/test/java/d/com/ecommerce/ProductControllerTest.java
+++ backend/src/test/java/d/com/ecommerce/ProductControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -23,6 +24,7 @@ import d.com.ecommerce.entity.Product;
 import d.com.ecommerce.service.ProductService;
 
 @WebMvcTest(ProductController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ProductControllerTest {
 
 	@Autowired
