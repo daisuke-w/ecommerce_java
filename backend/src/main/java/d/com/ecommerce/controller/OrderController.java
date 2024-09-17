6bfdd78344b3e76a57dfe25e72c774b2680c64e1
daisuke-w <w.daisuke.001@gmail.com>
2024-09-17 10:53:01 +0900

    OrderController作成

---
+++ backend/src/main/java/d/com/ecommerce/controller/OrderController.java
@@ -0,0 +1,36 @@
+package d.com.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import d.com.ecommerce.entity.Order;
+import d.com.ecommerce.entity.User;
+import d.com.ecommerce.service.OrderService;
+import d.com.ecommerce.service.UserService;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public Order createOrder(@AuthenticationPrincipal User user) {
+		return orderService.createOrder(user);
+	}
+	
+	@GetMapping("/{orderId}")
+	public Order getOrderById(@PathVariable Long orderId) {
+		return orderService.getOrderById(orderId);
+	}
+
+}
