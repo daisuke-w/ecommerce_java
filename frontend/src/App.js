d760565de117cbe7e79da345ab3a52b409f2bc7c
daisuke-w <w.daisuke.001@gmail.com>
2024-09-18 20:12:28 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ECサイト</h1>
-      </header>
-      <ProductList />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>ECサイト</h1>
+        </header>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
