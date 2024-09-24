0e007d087217868fcf058e5b056959f85185645b
daisuke-w <w.daisuke.001@gmail.com>
2024-09-24 18:30:42 +0900

    CORS設定

--- backend/src/main/java/d/com/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/d/com/ecommerce/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package d.com.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +18,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     	http
-    		.authorizeHttpRequests(authorize -> authorize
+			.cors(Customizer.withDefaults())
+			.authorizeHttpRequests(authorize -> authorize
     			.requestMatchers("/login", "/register").permitAll()
     			.anyRequest().authenticated()
     		)
