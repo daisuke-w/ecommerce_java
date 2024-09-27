486fdb890d6de20a7a1ec03fa85ff14439c9c20b
daisuke-w <w.daisuke.001@gmail.com>
2024-09-27 21:12:12 +0900

    @Component追記
    サーバー起動時にエラーとなる為
    SpringにBeanとして登録する為

--- backend/src/main/java/d/com/ecommerce/service/CustomUserDetailsService.java
+++ backend/src/main/java/d/com/ecommerce/service/CustomUserDetailsService.java
@@ -3,10 +3,12 @@ package d.com.ecommerce.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import d.com.ecommerce.entity.User;
 import d.com.ecommerce.repository.UserRepository;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 	
 	private final UserRepository userRepository;
