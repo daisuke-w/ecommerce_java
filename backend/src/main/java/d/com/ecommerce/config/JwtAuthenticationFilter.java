0ccbb94dc0561a154d219797f3f1e1178d30ee7a
daisuke-w <w.daisuke.001@gmail.com>
2024-09-27 21:11:49 +0900

    Create JwtAuthenticationFilter.java
    403 エラー解消の為
    認証トークン（JWTなど）がヘッダーに含まれていない、またはトークンが無効である場合に発生

---
+++ backend/src/main/java/d/com/ecommerce/config/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package d.com.ecommerce.config;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import d.com.ecommerce.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+	
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		try {
+			String path = request.getRequestURI();
+
+			if (path.equals("/api/users/login") || path.equals("/api/users/register")) {
+				filterChain.doFilter(request, response);
+				return;
+			}
+			
+			String jwt = jwtUtils.extractJwtFromRequest(request);
+			logger.info("Extracted JWT: " + jwt);
+			
+			if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+				String username = jwtUtils.getUsernameFromJwtToken(jwt);
+				logger.info("Username from JWT: " + username); 
+				UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+				
+				UsernamePasswordAuthenticationToken authentication =
+						new UsernamePasswordAuthenticationToken(
+								userDetails, null, userDetails.getAuthorities());
+				
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				logger.info("Authentication set for user: " + username);
+				filterChain.doFilter(request, response);
+			}
+		} catch (Exception e) {
+			logger.error("Cannot set user authentication: " + e.getMessage());
+		}
+	}
+}
