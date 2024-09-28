be703d79bb6f5ed784549ec71eb3b9fc77002e64
daisuke-w <w.daisuke.001@gmail.com>
2024-09-28 12:39:31 +0900

    AuthContext作成

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({}) => {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setAuth(!!token);
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem('token', token)
+    setAuth(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem(`token`)
+    setAuth(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ auth, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
