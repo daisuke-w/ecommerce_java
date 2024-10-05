d032289da90bf91488a66a88eee3b01dfa1df8ec
daisuke-w <w.daisuke.001@gmail.com>
2024-10-05 20:31:59 +0900

    画面更新時の挙動修正
    useEffrctの実行タイミングが画面描画後の為authが常にfalseになる問題を解決

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -3,7 +3,11 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-  const { auth } = useContext(AuthContext);
+  const { auth, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   if (!auth) {
     return <Navigate to="/login" replace />
