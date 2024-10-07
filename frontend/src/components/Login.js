ecc26731873998f082ee00a5a1008ee46ab85238
daisuke-w <w.daisuke.001@gmail.com>
2024-10-07 15:14:04 +0900

    修正漏れ

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     try {
       const response = await axios.post('/users/login', { username, password });
 
-      const { token, userId } = response.data.token;
+      const { token, userId } = response.data;
 
       if (token) {
         login(token, userId);
