91efb1f20a57ec6b16a14b6ca6bd6346a459e6a1
daisuke-w <w.daisuke.001@gmail.com>
2024-10-14 14:51:50 +0900

    Header css分離

--- frontend/src/components/Common/Header.js
+++ frontend/src/components/Common/Header.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from "../../context/AuthContext";
 import Logout from "../Auth/Logout";
 
+import styles from "./Header.module.css";
+
 const Header = () => {
   const { auth } = useContext(AuthContext);
 
   return (
-    <header className="App-header">
+    <header className={styles.header}>
       <div>
-        <h1><Link to="/" className="header-title">ECサイト</Link></h1>
+        <h1><Link to="/" className={styles.headerTitle}>ECサイト</Link></h1>
       </div>
       <div>
         {auth && (
           <>
-            <Link to="/cart" className="cart-button">カート</Link>
+            <Link to="/cart" className={styles.headerCart}>カート</Link>
             <Logout />
           </>
         )}
