282acb5e1a86d532fefe33eeb163bf529f44717b
daisuke-w <w.daisuke.001@gmail.com>
2024-10-20 18:14:51 +0900

    CartController修正

--- backend/src/main/java/d/com/ecommerce/controller/CartController.java
+++ backend/src/main/java/d/com/ecommerce/controller/CartController.java
@@ -2,7 +2,6 @@ package d.com.ecommerce.controller;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,16 +32,14 @@ public class CartController {
     @GetMapping
     public List<CartItem> getCartItems(Principal principal) {
     	String username = principal.getName();
-        Optional<User> userOpt = userService.getUserByUsername(username);
-        User user = userOpt.orElseThrow(() -> new RuntimeException("User not found"));
+    	User user = userService.getUserByUsername(username);
         return cartService.getCartItems(user);
     }
 
     @PostMapping("/add")
     public void addItemToCart(@RequestBody CartRequest cartRequest, Principal principal) {
     	String username = principal.getName();
-    	Optional<User> userOpt = userService.getUserByUsername(username);
-    	User user = userOpt.orElseThrow(() -> new RuntimeException("User not found"));
+    	User user = userService.getUserByUsername(username);
     	
     	Long productId = cartRequest.getProductId();
     	int quantity = cartRequest.getQuantity();
@@ -52,16 +49,14 @@ public class CartController {
     @DeleteMapping("/remove")
     public void removeItemFromCart(@RequestParam Long productId, Principal principal) {
     	String username = principal.getName();
-    	Optional<User> userOpt = userService.getUserByUsername(username);
-    	User user = userOpt.orElseThrow(() -> new RuntimeException("User not found"));
+    	User user = userService.getUserByUsername(username);
         cartService.removeItemFromCart(user, productId);
     }
 
     @PutMapping("/update")
     public void updateItemQuantity(@RequestParam Long productId, @RequestParam int quantity, Principal principal) {
     	String username = principal.getName();
-    	Optional<User> userOpt = userService.getUserByUsername(username);
-    	User user = userOpt.orElseThrow(() -> new RuntimeException("User not found"));
+    	User user = userService.getUserByUsername(username);
         cartService.updateItemQuantity(user, productId, quantity);
     }
 }
